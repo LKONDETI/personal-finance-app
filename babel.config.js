39c1535493261da89ad64392d0e3b2de82011fa8
Sravani K <sravanikondeti4@gmail.com>
2025-04-01 09:25:35 -0400

    removed main.py

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,9 @@ module.exports = function (api) {
   
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      require.resolve("expo-router/babel"), 
+      "nativewind/babel"
+    ],
   };
 };
